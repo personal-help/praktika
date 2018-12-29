2eb007f71582da6089c51011ae55305557a0e90e
helper <vovan.danchik@mail.ru>
2018-12-29 13:33:32 +0600

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,11 +1,9 @@
-var reg=0;
-
-$(window).on('load',function(){
+$(window).on('load', function() {
     var $predloader = $('#predloader');
     $predloader.delay(1650).fadeOut('slow');
 });
 
-$(document).ready(function(){   
+$(document).ready(function() {   
     $(".loader").circularProgress({
         color: "#00db8b",
         line_width: 25,
@@ -14,34 +12,44 @@ $(document).ready(function(){
         percent: 0,
     }).circularProgress('animate', 100, 1500);
     
-    for(var k=1;k<=4;k++){ topMenu("#part"+k,"#menu"+k,k); }
+    for(var k=1;k<=4;k++) { topMenu("#part"+k,"#menu"+k,k); }
     
     for(var k=1;k<=6;k++){
-        for(var l=1;l<=6;l++){
+        for(var l=1;l<=6;l++) {
             menu("#m"+k+"part"+l,"#c"+k+"part"+l,l,k);
         }
     }
     
-        $(".login").bind("click", function(){
+        $(".login").bind("click", function() {
             $(".cols.col-logMenu").slideDown(500);
         });
 
-        $(".buttIn").bind("click", function(){
+        $(".buttIn").bind("click", function() {
             $(".cols.col-logMenu").slideUp(500);
         });   
     
+        $("#part5").bind("click", function() {
+            $("#news").slideDown('slow');
+            
+            for(var j=1;j<=6;j++){
+                for(var i=1;i<=6;i++){
+                    $("#c"+j+"part"+i).slideUp('slow');
+                    topMenu("#part"+5,"#menu"+5,5);
+                }
+            }
+        });
 });
 
-function topMenu(thisName,forName,num){
+function topMenu(thisName,forName,num) {
     $(thisName).bind("click", function(){ for(var i=1;i<=5;i++){if(i!=num){
         if($("#menu"+i)!=""){ $("#menu"+i).hide('fast'); } $(forName).slideDown('slow');}}});
 }
 
-function menu(thisName,forName,num,num2){        
-    $(thisName).bind("click",function(){ 
+function menu(thisName,forName,num,num2) {        
+    $(thisName).bind("click",function() { 
         $(forName).slideDown('slow');
         for(var j=1;j<=6;j++){
-            for(var i=1;i<=6;i++){if(i!=num || j!=num2){
-                if($("#c"+j+"part"+i)!=""){ $("#c"+j+"part"+i).slideUp('slow');} 
+            for(var i=1;i<=6;i++){if(i!=num || j!=num2) {
+                if($("#c"+j+"part"+i)!="") { $("#news").slideUp('slow'); $("#c"+j+"part"+i).slideUp('slow');} 
     $(forName).slideDown('slow');}}}});  
 }
